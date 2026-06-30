2937a27a531788fcf886ad9fe55a611004242dee
sniperreid <82688767+sniperreid@users.noreply.github.com>
2026-06-30 16:16:40 -0500

    Refactor zTable methods and add new functionalities
    Refactor zTable methods to improve readability and maintainability by removing unnecessary whitespace and adding new functions for length and payload size management.

--- zTable/init.lua
+++ zTable/init.lua
@@ -5,15 +5,15 @@ local HttpService = game:GetService("HttpService")
 
 local function deepCopy(original)
 	local copy = {}
-	
+
 	for key, value in original do
 		if typeof(value) == "table" then
 			value = deepCopy(value)
 		end
-		
+
 		copy[key] = value
 	end
-	
+
 	return copy
 end
 
@@ -21,14 +21,14 @@ local function deepSearch(original, scope, position)
 	for i, v in original do
 		if position and typeof(v) ~= "table" then continue end
 		if typeof(v) == "table" and v._handlerListHead ~= nil then continue end
-		
+
 		if position and typeof(v) == "table" and v.ref and v.ref[position] == scope or v == scope then
 			return i, v
 		end
-		
+
 		if typeof(v) == "table" and v.ref then
 			local searched = deepSearch(v.ref, scope, position)
-			
+
 			if searched then
 				return i, searched
 			end
@@ -40,7 +40,7 @@ local function shallowSearch(original, scope, position)
 	for i, v in original do
 		if position and typeof(v) ~= "table" then continue end
 		if typeof(v) == "table" and v._handlerListHead ~= nil then continue end
-		
+
 		if position and typeof(v) == "table" and v.ref and v.ref[position] == scope or v == scope then
 			return i, v
 		end
@@ -55,7 +55,7 @@ zTable.__index = zTable
 -- Creates a new zTable object.
 function zTable.new(ref): zTableMalleable
 	ref = deepCopy(ref or {})
-	
+
 	local formedTable = setmetatable({}, zTable)
 
 	formedTable.Destroying = Signal.new()
@@ -66,6 +66,7 @@ function zTable.new(ref): zTableMalleable
 
 	formedTable.ClassName = "zTable"
 	formedTable.ref = zTable.transformReference(ref)
+	formedTable.length = #formedTable:raw()
 
 	return formedTable
 end
@@ -85,9 +86,9 @@ export type formedTable = typeof(zTable.new {})
 -- Fully destroys the zTable object
 function zTable:Destroy()
 	self.Destroying:Fire()
-	
+
 	self:Clear()
-	
+
 	table.clear(self)
 	setmetatable(self, nil)
 end
@@ -95,14 +96,14 @@ end
 -- Clears the zTable metadata
 function zTable:Clear()
 	self.Clearing:Fire()
-	
+
 	table.clear(self.ref)
 end
 
 -- Returns the raw table from zTable metadata
 function zTable:raw()
 	local rawTable = {}
-	
+
 	for i, v in self.ref do
 		if typeof(v) == "table" and v.ClassName == "zTable" then
 			rawTable[i] = v:raw()
@@ -110,46 +111,54 @@ function zTable:raw()
 			rawTable[i] = v
 		end
 	end
-	
+
 	return rawTable
 end
 
 -- Inserts object into zTable metadata
 function zTable:Insert(Value: any?): zTableMalleable
-	Value = Value or {}
+	if self.payloadLength and self.payloadLength >= self.length + 1 then
+		return
+	end
 	
+	Value = Value or {}
+
 	if typeof(Value) == "table" and Value.ClassName ~= "zTable" then
 		Value = zTable.new(Value)
 	end
-	
+
 	if typeof(Value) == "table" and Value.ref then
 		self.Inserting:Fire(#self.ref + 1, Value.ref)
 	else
 		self.Inserting:Fire(#self.ref + 1, Value)
 	end
 	
+	self.length += 1
+
 	table.insert(self.ref, Value)
 	
+	self:enforcePayloadLength()
+
 	return Value
 end
 
 -- Sets value in zTable metadata at key
 function zTable:Set(key: string, Value: any?): zTableMalleable
 	Value = Value or {}
-	
+
 	if typeof(Value) == "table" and Value.ClassName ~= "zTable" then
 		Value = zTable.new(Value)
 	end
-	
+
 	-- is a zTable
 	if typeof(Value) == "table" and Value.ClassName == "zTable" then
 		self.Changing:Fire(key, Value.ref)
 	else
 		self.Changing:Fire(key, Value)
 	end
-	
+
 	self.ref[key] = Value
-	
+
 	return Value
 end
 
@@ -166,24 +175,31 @@ end
 -- Pops an object in zTable metadata at x position
 function zTable:Pop(position: number?)
 	if not position then return end
-	
+
 	local object
-	
+
 	if typeof(position) == "number" then
 		object = table.remove(self.ref, position)
+		
+		if object then
+			self.length -= 1
+		end
+		
 	else
 		object = self.ref[position]
 
 		self.ref[position] = nil
 	end
 	
+	if not object then return end
+
 	-- is a zTable
 	if typeof(object) == "table" and object.ClassName == "zTable" then
 		self.Removing:Fire(position, object.ref)
 	else
 		self.Removing:Fire(position, object)
 	end
-	
+
 	return object
 end
 
@@ -195,22 +211,28 @@ function zTable:ForEach(Callback: (any) -> ())
 end
 
 -- Loops through shallow objects (reverse order) and fires the callback with item info.
-function zTable:ReverseSearch(Callback: (any) -> ())
+function zTable:ReverseSearch(Callback: (any) -> (), startingPoint: number?, endPoint: number?)
 	local z = self:raw()
-	
-	for i = #z, 1, -1 do
+
+	for i = startingPoint or #z, endPoint or 1, -1 do
 		Callback(self, i, z[i])
 	end
 end
 
 -- Fills a new zTable metadata with x number of objects.
 function zTable:Create(count: number, objects: any): formedTable
+	-- don't use packed arguments for type casting.
 	return zTable.new( table.create(count, objects) )
 end
 
 -- Creates a new zTable with the same data as current table.
 function zTable:Clone(): formedTable
-	return zTable.new(self:raw())
+	local formedTable = zTable.new(self:raw())
+	
+	formedTable.length = self.length
+	formedTable:setPayloadSize(self.payloadSize)
+	
+	return formedTable
 end
 
 -- Recursively search through every element of metadata or branch to find an object.
@@ -233,17 +255,17 @@ end
 -- Increments a value in metadata by x or 1
 function zTable:Increment(key: string | number, amount: number?)
 	amount = amount or 1
-	
+
 	local value = self:Get(key)
-	
+
 	assert(value, `{key} is not a valid member of {self.ClassName}`)
-	
+
 	local vT = typeof(value)
 	local aT = typeof(amount)
-	
+
 	assert(aT == "number", `number expected when adding value to {key}, got {aT}`)
 	assert(vT == "number", `number expected when indexing Increment, got {vT}`)
-	
+
 	return self:Set(key, value + amount)
 end
 
@@ -255,6 +277,31 @@ end
 -- Unpacks object
 function zTable:Unpack()
 	return table.unpack(self.ref)
+end
+
+-- Get length of array without calling raw metadata
+function zTable:len()
+	return self.length
+end
+
+-- Set zTable payload max length
+function zTable:setPayloadSize(payloadSize: number)
+	self.payloadSize = payloadSize
+	
+	self:enforcePayloadLength()
+end
+
+-- Refresh payload length
+function zTable:enforcePayloadLength()
+	local payloadSize = self.payloadSize
+	
+	if not payloadSize then return end
+	
+	self:ReverseSearch(
+		self.Pop,
+		self:len(),
+		payloadSize + 1
+	)
 end
 
 export type zTableMalleable = typeof(setmetatable({} :: formedTable, zTable))
